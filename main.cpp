c9ef6618c5b332df926639eb4a277d6f2922221e
Vishwaja <vishwajanakka@gmail.com>
2025-04-28 10:50:25 +0530

    secondary commit

--- main.cpp
+++ main.cpp
@@ -11,3 +11,23 @@ class order;
     order(int id;string item; string payment; int time)
         :tokenID(id), itemType(item), paymentType(payment), takingTime(time) {}
 };
+class QueueManager {
+    priavte:
+        queue<order> beverageQueue;
+        queue<order> lunchQueue;
+    public:
+        void addOrder(const Order& object order){
+            if(order.iteamType == "beverage"){
+                beverageQueue.push(order);
+                cout<<" added tokenID:"<<order.tokenID<< "to the beverage counter \n";
+            }
+            else if (order.itemType =="lunch"){
+                lunchQueue.push(order);
+                cout<<"added tokenID"<< order.tokenID<< "to the lunch counter\n";
+            }
+            else{
+                cout<< "Invalid order type.\n";
+            }
+            }
+        }
+}
